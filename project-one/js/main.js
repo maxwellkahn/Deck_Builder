52c2be7bac24806be6b4c5e35442f9f53003e1e5
maxwelljkahn <maxwelljkahn@gmail.com>
2021-06-22 15:53:03 -0700

    removed make and delete functions in favore of adding and removing event listeners

--- project-one/js/main.js
+++ project-one/js/main.js
@@ -48,14 +48,18 @@ let score, result, state
 /*---- cached element references ----*/
 const startBtn = document.querySelector('#startHere')
 
-const gameButtons = document.querySelector('#buttons')
-// const gameSlotOne =
-// const gameSlotTwo = 
-// const gameslotThree = 
+// const gameButtons = document.querySelector('#buttons')
+const playBtn = document.querySelector('#playBtn')
+const stopBtn = document.querySelector('#stopBtn')
+
+const gameSlotOne = document.querySelector('#slot1')
+const gameSlotTwo = document.querySelector('#slot2')
+const gameslotThree = document.querySelector('#slot3')
 // these will be used when they are created by the 'playGame' function
-const stopBtnOne = document.querySelector('#stop1')
-const stopBtnTwo = document.querySelector('#stop2')
-const stopBtnThree = document.querySelector('#stop3')
+const StopBtn = document.querySelector('#stopButton')
+// const stopBtnOne = document.querySelector('#stop1')
+// const stopBtnTwo = document.querySelector('#stop2')
+// const stopBtnThree = document.querySelector('#stop3')
 
 const scoreBoard = document.querySelector('#score')
 
@@ -85,33 +89,50 @@ function initialize() {
         slotThree: 'insertCoin'
     }
     
-    deleteStart()
-    makePlayBtn()
+    startBtn.removeEventListener('click', initialize)
+    playBtn.addEventListener('click', playGame)
     render()
 }
-function deleteStart() {
-    startBtn.parentNode.removeChild(startBtn)
-}
-function makePlayBtn() {
-    let playBtnEl = document.createElement('button');
-    playBtnEl.textContent = 'Play';
-    playBtnEl.setAttribute('id', 'playBtn');
-    gameButtons.appendChild(playBtnEl);
-    playBtn.addEventListener('click', playGame)
-}
+
+// removed in favor of .removeEventListener()
+// function deleteStart() {
+//     // deletes start button, can only be renabled when score goes below 0 (if score > 0)
+//     startBtn.parentNode.removeChild(startBtn)
+// }
+
+// function makePlayBtn() {
+//     // creates play button to enable game start
+//     let playBtnEl = document.createElement('button');
+//     playBtnEl.textContent = 'Play';
+//     playBtnEl.setAttribute('id', 'playBtn');
+//     gameButtons.appendChild(playBtnEl);
+//     playBtn.addEventListener('click', playGame)
+// }
+
+// removed in favor of .removeEventListener()
+// function deletePlayBtn(){
+//     // removes play button so it cannot be repetedly pressed
+//     let playBtn = document.querySelector('#playBtn')
+//     playBtnEl.parentNode.removeChild(playBtn)
+// }
+
 function playGame() {
     //sets slot state to 'spinning' & allows stop buttons to be pressed
+    let playBtn = document.querySelector('#playBtn');
+    playBtn.removeEventListener('click', playGame);
     score.player--; 
-    let gameBtnEl = document.createElement('button');
-    gameBtnEl.textContent = 'Stop';
-    gameBtnEl.setAttribute('id', 'stopButton');
-    gameButtons.appendChild(gameBtnEl)
+    stopBtn.addEventListener('click', stopSpinning)
+    // let gameBtnEl = document.createElement('button');
+    // gameBtnEl.textContent = 'Stop';
+    // gameBtnEl.setAttribute('id', 'stopButton');
+    // gameButtons.appendChild(gameBtnEl)
+    // stopBtn.addEventListener('click', stopSpinning)
     render()
 // need to create THREE stop buttons
 
-    gameSlotOne.style.backgroundImage = `url(${slotSymbols[spinning.slotOne].imgUrl})`
-    gameSlotTwo.style.backgroundImage = `url(${slotSymbols[spinning.slotTwo].imgUrl})`
-    gameslotThree.style.backgroundImage = `url(${slotSymbols[spinning.slotThree].imgUrl})`
+    // gameSlotOne.style.backgroundImage = `url(${slotSymbols[spinning.slotOne].imgUrl})`
+    // gameSlotTwo.style.backgroundImage = `url(${slotSymbols[spinning.slotTwo].imgUrl})`
+    // gameslotThree.style.backgroundImage = `url(${slotSymbols[spinning.slotThree].imgUrl})`
 
 }
 
@@ -120,11 +141,14 @@ function getRandomIdx() {
     //used to generate a number that will corespond with slot symbols
 }
 
-// function stopSpinning() {
-//     //changes the state of slot from 'spinning' to 'not spinning'
-//     result.slots = lookUp[getRandomIdx()]
-//     removeBtn()
-// } 
+function stopSpinning() {
+    result.gameSlotOne = lookUp[getRandomIdx()]
+    result.gameSlotTwo = lookUp[getRandomIdx()]
+    result.gameslotThree = lookUp[getRandomIdx()]
+    render()
+    console.log(result.gameSlotOne, result.gameSlotTwo, result.gameslotThree)
+    checkSlots()
+} 
 
 
 // function removeBtn() {
@@ -134,14 +158,19 @@ function getRandomIdx() {
 
 function checkSlots() {
     //to see if any points are awarded
+    stopButton.removeEventListener('click', stopSpinning)
+    if (result.gameSlotOne === result.gameSlotTwo && result.gameSlotOne === result.gameslotThree) {
+        score.player += 50
+    } else alert('try again!');
+    playBtn.addEventListener('click', playGame)
 }
 
 function render() {
     //whenever game state is changed to render app state
     scoreBoard.textContent = score.player
 
-    gameSlotOne.style.backgroundImage = `url(${slotSymbols[result.slotOne].imgUrl})`
-    gameSlotTwo.style.backgroundImage = `url(${slotSymbols[result.slotTwo].imgUrl})`
-    gameSlotThree.style.backgroundImage = `url(${slotSymbols[result.slotThree].imgUrl})`
+    // gameSlotOne.style.backgroundImage = `url(${slotSymbols[result.slotOne].imgUrl})`
+    // gameSlotTwo.style.backgroundImage = `url(${slotSymbols[result.slotTwo].imgUrl})`
+    // gameSlotThree.style.backgroundImage = `url(${slotSymbols[result.slotThree].imgUrl})`
 }
 
