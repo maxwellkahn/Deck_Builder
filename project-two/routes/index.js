d85f20a9225adcfd0912e0b8333626038c44589b
maxwelljkahn <maxwelljkahn@gmail.com>
2021-07-10 10:50:35 -0700

    attempts to display card information on landing page

--- project-two/routes/index.js
+++ project-two/routes/index.js
@@ -1,9 +1,21 @@
+const { name } = require('ejs');
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+
+// const rootURL = 'https://api.scryfall.com/';
+const rootURL = 'https://api.scryfall.com/cards/random/?q=in%3Aleg'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const cardData = req.query.body;
+  request(
+    `${rootURL}`,
+    function(err, response, body) {
+      res.render('index', {cardData: object.name});
+    }
+  );
+  // console.log('cardname: ', req.query.cardName)
 });
 
 module.exports = router;
