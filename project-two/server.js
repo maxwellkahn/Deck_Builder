273257699ae31c425aa8bb6a5fbff3d29d477070
maxwelljkahn <maxwelljkahn@gmail.com>
2021-07-12 10:25:51 -0700

    adds connection to cloud database

--- project-two/server.js
+++ project-two/server.js
@@ -3,11 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv').config();
+require('./config/database');
 
 const indexRouter = require('./routes/index');
 const decksRouter = require('./routes/decks');
 
 const app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
